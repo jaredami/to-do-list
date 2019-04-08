9967b9236aab8d6c0632fabd322e90b7d238f9d0
jaredami <jarstorm@imail.iu.edu>
2019-04-07 22:09:56 -0400

    added tests for login reducer

--- src/reducers/reducers.test.js
+++ src/reducers/reducers.test.js
@@ -1,6 +1,7 @@
 import darkModeReducer from "./darkModeReducer";
 import loginReducer from "./loginReducer";
 
+/* tests for dark mode toggler reducer */
 describe("dark mode reducer", () => {
   it("should return the initial state", () => {
     expect(darkModeReducer(undefined, {})).toEqual({
@@ -60,6 +61,108 @@ describe("dark mode reducer", () => {
         secondaryDark: "#8e4154",
         accent: "#ffb998"
       }
+    });
+  });
+});
+
+/* tests for login reducer */
+describe("login reducer", () => {
+  it("should return the initial state", () => {
+    expect(loginReducer(undefined, {})).toEqual({
+      loggedIn: true,
+      usernameValue: "",
+      passwordValue: ""
+    });
+  });
+
+  it("should handle USERNAME_INPUT_CHANGE", () => {
+    expect(
+      loginReducer(undefined, {
+        type: "USERNAME_INPUT_CHANGE",
+        usernameInput: "test"
+      })
+    ).toEqual({
+      loggedIn: true,
+      usernameValue: "test",
+      passwordValue: ""
+    });
+
+    expect(
+      loginReducer(
+        {
+          loggedIn: true,
+          usernameValue: "before",
+          passwordValue: ""
+        },
+        {
+          type: "USERNAME_INPUT_CHANGE",
+          usernameInput: "after"
+        }
+      )
+    ).toEqual({
+      loggedIn: true,
+      usernameValue: "after",
+      passwordValue: ""
+    });
+  });
+
+  it("should handle PASSWORD_INPUT_CHANGE", () => {
+    expect(
+      loginReducer(undefined, {
+        type: "PASSWORD_INPUT_CHANGE",
+        passwordInput: "test"
+      })
+    ).toEqual({
+      loggedIn: true,
+      usernameValue: "",
+      passwordValue: "test"
+    });
+
+    expect(
+      loginReducer(
+        {
+          loggedIn: true,
+          usernameValue: "",
+          passwordValue: "before"
+        },
+        {
+          type: "PASSWORD_INPUT_CHANGE",
+          passwordInput: "after"
+        }
+      )
+    ).toEqual({
+      loggedIn: true,
+      usernameValue: "",
+      passwordValue: "after"
+    });
+  });
+
+  it("should handle LOGIN_SUBMIT", () => {
+    expect(
+      loginReducer(undefined, {
+        type: "LOGIN_SUBMIT"
+      })
+    ).toEqual({
+      loggedIn: false,
+      usernameValue: "",
+      passwordValue: ""
+    });
+
+    expect(
+      loginReducer(
+        {
+          loggedIn: false,
+          usernameValue: "yaredami@gmail.com",
+          passwordValue: "pw"
+        },
+        {
+          type: "LOGIN_SUBMIT"
+        }
+      )
+    ).toEqual({
+      loggedIn: true,
+      usernameValue: "yaredami@gmail.com",
+      passwordValue: "pw"
     });
   });
 });
